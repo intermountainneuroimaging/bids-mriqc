ebb0c25df00d6df8c8c3be3c4ac9d57bf9df643d
Brianne Sutton <21200969+neurosutton@users.noreply.github.com>
2021-05-13 09:09:39 -0600

    Add debug messages, search directories

--- utils/results/store_iqms.py
+++ utils/results/store_iqms.py
@@ -33,22 +33,37 @@ def store_iqms(hierarchy, output_analysis_id_dir):
         return metadata
 
 def _find_files(hierarchy, output_analysis_id_dir):
+    """
+    Locates analysis output. Assumes naming scheme follows BIDS format.
+        hierarchy (dict): from get_run_level_and_hierarchy ultimately. Dictionary
+        describes aspects of the analysis, including project, participant, or session
+        level used here to differentiate where to look for jsons.
+        output_analysis_id_dir (path): path including the destination id for project
+        level analyses; avoids internal call to get the destination id of the container.
+    Raises:
+        IndexError: If no subject-level jsons are available in the path specified,
+        there is no data that can be harvested for metadata. May need to check the
+        path or see if the analysis was not completed.
+    """
     if hierarchy['run_level'] == 'project':
         path_to_jsons = os.path.join(
             output_analysis_id_dir,
             'sub*',
             'ses*',
-            "*/*.json"
+            "*/sub*.json"
         )
     else:
-        path_to_jsons = os.path.join(output_analysis_id_dir, '*.json')
-    jsons = glob(path_to_jsons)
+        output_dir = os.path.dirname(os.path.normpath(output_analysis_id_dir))
+        path_to_jsons = os.path.join(output_dir, 'sub*.json')
+    jsons = glob(path_to_jsons, recursive=True)
     try:
         # Throw IndexError for no files
         jsons[0]
+        log.info(f'Found {jsons}')
     except IndexError:
         log.info('Did not find MRIQC output jsons to harvest.')
-        log.debug(f"Missing info for metadata. Checked here: \n {path_to_jsons}")
+        log.debug(f"Missing info for metadata. Checked here: \n {path_to_jsons}\n"
+                  f"{glob(os.path.join(output_dir,'*'), recursive=True)}")
 
 def _create_nested_metadata(analysis_to_parse):
     """
@@ -60,6 +75,7 @@ def _create_nested_metadata(analysis_to_parse):
         add_metadata (nested dict): dictionary to append to metadata under the analysis >
         info > sorting_classifier (filename) entry
     """
+
     toss_keys = [k for k in analysis_to_parse.keys() if k.startswith("__")]
     toss_keys.extend(["bids_meta", "provenance"])
 
@@ -76,4 +92,5 @@ def _create_nested_metadata(analysis_to_parse):
                 add_metadata.setdefault(labels[0], {})[labels[1]] = v
             else:
                 add_metadata[labels[0]] = v
+    log.debug(f'Passing {len(add_metadata)} IQM items to metadata.')
     return add_metadata
